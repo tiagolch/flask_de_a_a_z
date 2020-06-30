60251ac65b2f7878a314eb6c3f3c51706f9fa0e8
Tiago <tiagolch@hotmail.com>
2020-06-29 21:27:35 -0300

    primeiro commit, ambiente configurado

---
+++ hello_world/livro_flask/run.py
@@ -0,0 +1,14 @@
+import sys
+from app import create_app
+from config import app_active, app_config
+
+config = app_config[app_active]
+config.APP = create_app(app_active)
+
+if __name__ == '__main__':
+    config.APP.run(host=config.IP_HOST, port=config.PORT_HOST)
+    reload(sys)
+
+
+
+
